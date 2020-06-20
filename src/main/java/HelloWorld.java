a03b5611e14de8812a49aa2f02514c13599acb12
LaiXingYu <1546431565@qq.com>
2020-06-20 17:12:49 +0800

    delete files

--- src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author DebugMouse
- * @date 2020/6/20 16:57
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
